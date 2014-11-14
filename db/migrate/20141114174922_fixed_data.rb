e2386626e2d4c89770d0cc8fcd9a4005a8899494
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-14 10:54:02 -0700

    fixed db

---
+++ db/migrate/20141114174922_fixed_data.rb
@@ -0,0 +1,11 @@
+class FixedData < ActiveRecord::Migration
+  def change
+    Organization.where(name: nil).delete_all
+    Organization.where(name: "").delete_all
+    Organization.where(name: " ").delete_all
+
+    Organization.where(active: nil).update_all(active: false)
+
+    Person.where("date_of_birth < ?", 100.years.ago).delete_all
+  end
+end
